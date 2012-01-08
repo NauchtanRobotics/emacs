78e8255ae8f73c6090fe7a3b85e2fdb8f0e228d4
Ben Davies <brdavies@gmail.com>
2012-01-09 08:36:32 +1000

    Added taskjuggler mode and added snippets for C.

--- rc/rc-taskjuggler.el
+++ rc/rc-taskjuggler.el
@@ -4,5 +4,6 @@
 
 (autoload 'taskjuggler-mode "taskjuggler-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.tjp" . taskjuggler-mode))
+(add-to-list 'auto-mode-alist '("\\.tji" . taskjuggler-mode))
 
 (add-hook 'taskjuggler-mode-hook 'ben-taskjuggler-mode-hook)
