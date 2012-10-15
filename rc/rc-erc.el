dd214cd5426ce1060116c03213a8e1777cc4043c
Ben Davies <brdavies@gmail.com>
2012-10-16 09:34:37 +1000

    Added erc notifications.

---
+++ rc/rc-erc.el
@@ -0,0 +1,8 @@
+;; Notifications for ERC messages (gchat, hotmail, etc). These are fairly
+;; intrusive so I've disabled them.
+
+;; (autoload 'erc-notifications "erc-notifications" nil t)
+
+;; (require 'erc-notifications)
+
+;; (add-to-list 'erc-modules 'notifications)
