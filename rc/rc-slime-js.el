d2f8ef23d312d7eb7dba436cc410c445d5f8cdbf
Ben Davies <brdavies@gmail.com>
2012-12-12 14:53:33 +1000

    Local version of slime-js now works.

--- rc/rc-slime-js.el
+++ rc/rc-slime-js.el
@@ -1,11 +1,11 @@
 ;; Why the mother fuck is this required?
 ;;(add-to-list 'load-path (concat dotfiles-dir "/elpa/slime-js-0.0.1"))
 
-;; (require 'slime-js nil 'noerror)
-
 ;; (add-hook 'after-init-hook
 ;;           #'(lambda ()
 ;;               (when (locate-library "slime-js")
 ;;                 (require 'setup-slime-js))))
 
+(require 'slime)
+(require 'slime-js nil 'noerror)
 (require 'setup-slime-js nil 'noerror)
