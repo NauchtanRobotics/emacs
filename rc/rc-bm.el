d0e73289f01b7966cf3f5d8fa70fdbe711f773b2
Ben Davies <brdavies@gmail.com>
2013-04-17 11:54:48 +1000

    New shortcuts for visual bookmarks.

--- rc/rc-bm.el
+++ rc/rc-bm.el
@@ -5,3 +5,7 @@
 (global-set-key (kbd "<C-f2>") 'bm-toggle)
 (global-set-key (kbd "<f2>") 'bm-next)
 (global-set-key (kbd "<S-f2>") 'bm-previous)
+
+(global-set-key (kbd "C-c C-t") 'bm-toggle)
+(global-set-key (kbd "C-c t") 'bm-next)
+(global-set-key (kbd "C-c T") 'bm-previous)
