cc55e8353230d19a2cbace9dc619698bceca549d
Ben Davies <brdavies@gmail.com>
2013-05-15 17:24:52 +1000

    Use json-mode for .json files.

--- rc/rc-json-mode.el
+++ rc/rc-json-mode.el
@@ -1,5 +1,7 @@
 (autoload 'json-mode "json-mode" nil t)
 
+(add-to-list 'auto-mode-alist '("\\.json" . json-mode))
+
 ;; Disable auto-fill mode, it really buggers things up.
 (add-hook 'json-mode-hook
           (lambda()
