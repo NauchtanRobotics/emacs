1343543a448c7fdedba146507557a5dd75c94dbc
Ben Davies <brdavies@gmail.com>
2013-09-17 13:19:54 +1000

    Added fundamental ansi mode.

---
+++ packages/fundamental-ansi.el
@@ -0,0 +1,4 @@
+(define-derived-mode fundamental-ansi-mode fundamental-mode "fundamental ansi"
+  "Fundamental mode that understands ansi colors."
+  (require 'ansi-color)
+  (ansi-color-apply-on-region (point-min) (point-max)))
