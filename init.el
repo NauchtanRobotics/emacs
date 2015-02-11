30e8db7fc0e61e3b4b2e9cb124825b9df7fef4ad
Ben Davies <brdavies@gmail.com>
2015-02-11 10:29:15 +1000

    Removed magit.

--- init.el
+++ init.el
@@ -5,6 +5,7 @@
           ("ELPA" . "http://tromey.com/elpa/")
           ("gnu" . "http://elpa.gnu.org/packages/")
           ("marmalade" . "http://marmalade-repo.org/packages/")
+          ("melpa" . "http://melpa.org/packages/")
           ))
   (package-initialize)
   )
